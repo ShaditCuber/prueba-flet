b0cfffb1798418067b53b0564f867fec3fa0b3e5
Felipe Bastidas <64512945+ShaditCuber@users.noreply.github.com>
2023-02-26 16:17:03 -0300

    runtime

--- main.py
+++ main.py
@@ -1,102 +1,50 @@
-
-from flet import *
-import cv2
-  
-VID=None
-MORIR=False
+import flet as ft
 
 
-def main(page:Page):
-    def cerrar(e):
-        global MORIR
-        
-        MORIR=True
-        # After the loop release the cap object
-        
-        
-        page.clean()
-        btn_rp=FilledButton(text="Generar Registro Presencial",width=300,height=45,on_click=btn_click)
-        btn_id=FilledButton(text="Generar Informe y Diplomas",width=300,height=45)
-        
-        # login=TextField(label="Nombre Usuario",hint_text="user")
-        # password=TextField(label="Contraseña",hint_text="pass",password=True,can_reveal_password=True)
-        
-        page.appbar=AppBar(title=Text("Izied"),bgcolor="#6ec63b",center_title=True)
-        page.add(
-            Container(
-                content=Column([
-                img,
-                btn_rp,
-                btn_id
-                ],spacing=40)
-            )
-            )
-        
-    def btn_click(e):
-        global MORIR
-        page.clean()
-        VID = cv2.VideoCapture(0)
-        
-        btn_rp=FilledButton(text="X",width=45,height=45,on_click=cerrar)
-        
-        
-        page.add(
-        Container(
-            content=Column([
-                btn_rp
-            ],spacing=40)
-        )
-        )
-        # define a video capture object
-        MORIR=False
-        while(True):
-            
-            # Capture the video frame
-            # by frame
-            ret, frame = VID.read()
+def main(page: ft.Page):
+    # the title of the app
+    page.title = "Flet Counter App"
 
-            
-            
-            # Display the resulting frame
-            cv2.imshow('frame', frame)
-            # the 'q' button is set as the
-            # quitting button you may use any
-            # desired button of your choice
-            key=cv2.waitKey(1)
-            if  key==ord("q") or MORIR:
-                break
-        
-        VID.release()
-        # Destroy all the windows
-        cv2.destroyAllWindows()
-  
-        
-    
-    page.window_width=300.00
-    page.window_height=720.00
-    img=Image(
-        src=f"izi.ico",
-        fit="contain",
-        width=300,
-        height=100
+    # a light/bright theme
+    page.theme_mode = "light"
+
+    # use material 2 design theme | this is just to better mimic the Flutter example
+    page.theme = ft.Theme(use_material3=False)
+
+    # the page's alignment
+    page.horizontal_alignment = "center"
+    page.vertical_alignment = "center"
+
+    def increment_counter(e):
+        """Increment the value of the counter_text object by 1, and update the UI to reflect these changes."""
+        counter_text.value = str(int(counter_text.value) + 1)
+        page.update()
+
+    # the app's appbar
+    page.appbar = ft.AppBar(
+        title=ft.Text("Flet Demo Home Page", color=ft.colors.WHITE),  # a title of white color
+        bgcolor=ft.colors.BLUE,  # a blue background color
+        center_title=True  # center the title || without this, the title will be on the left
     )
-    btn_rp=FilledButton(text="Generar Registro Presencial",width=300,height=45,on_click=btn_click)
-    btn_id=FilledButton(text="Generar Informe y Diplomas",width=300,height=45)
-    
-    # login=TextField(label="Nombre Usuario",hint_text="user")
-    # password=TextField(label="Contraseña",hint_text="pass",password=True,can_reveal_password=True)
-    
-    page.appbar=AppBar(title=Text("Izied"),bgcolor="#6ec63b",center_title=True)
+
+    # text that contains the counter number to be incremented
+    counter_text = ft.Text("0", style=ft.TextThemeStyle.DISPLAY_MEDIUM)
+
+    # the app's FAB
+    page.floating_action_button = ft.FloatingActionButton(
+        content=ft.Icon(ft.icons.ADD, color=ft.colors.WHITE),
+        shape=ft.CircleBorder(),  # gives the button a round/circle shape
+        on_click=increment_counter,  # the callback to be executed when this button is clicked
+        tooltip="Increment",  # the text to be shown when this button is hovered
+        bgcolor=ft.colors.BLUE  # a blue background color
+    )
+
+    # adding our widgets/controls to the page/UI
     page.add(
-        Container(
-            content=Column([
-            img,
-            btn_rp,
-            btn_id
-            ],spacing=40)
-        )
-        )
+        ft.Text("You have pushed the button this many times:"),
+        counter_text
+    )
 
 
-app(target=main)
-# app(target=main,port=8000,view=WEB_BROWSER)
+# open a browser tab containing the app | remove the view parameter to open in a native OS window
+ft.app(target=main, view=ft.WEB_BROWSER)
